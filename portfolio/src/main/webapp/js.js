2822dc79ffd6e19af92a3aef98c772679750943a
ConstantineWang <cwang@sps-program.com>
2022-06-04 15:19:42 +0000

    Add initial static front-end page

---
+++ portfolio/src/main/webapp/js.js
@@ -0,0 +1,20 @@
+var tags = document.getElementsByClassName("row")
+console.log(tags)
+
+for (let i = 0; i < tags.length; i++) {
+
+    tags[i].addEventListener("click", function () {
+        console.log(this)
+        for (let i = 1; i < this.children.length; i++) {{
+                if (this.children[i].style.display == "block") {
+                    this.children[i].style.display = "none"
+                }
+                else {
+                    this.children[i].style.display = "block"
+                }   
+            }
+        }
+
+    })
+
+}
