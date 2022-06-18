b2d87df6fa74a0e41a06ed72e0c2440eb75a7482
ConstantineWang <cwang@sps-program.com>
2022-06-18 10:10:10 +0000

    Add Datastore library to store messages

--- portfolio/src/main/java/com/google/sps/servlets/GetTextServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/GetTextServlet.java
@@ -1,10 +1,17 @@
 package com.google.sps.servlets;
 
+import com.google.cloud.datastore.Datastore;
+import com.google.cloud.datastore.DatastoreOptions;
+import com.google.cloud.datastore.Entity;
+import com.google.cloud.datastore.FullEntity;
+import com.google.cloud.datastore.KeyFactory;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.jsoup.Jsoup;
+import org.jsoup.safety.Whitelist;
 
 /** Handles requests sent to the /hello URL. Try running a server and navigating to /hello! */
 @WebServlet("/contact")
@@ -13,15 +20,30 @@ public class GetTextServlet extends HttpServlet {
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
     // Get the value entered in the form.
-    String name = request.getParameter("name");
+    String name = Jsoup.clean(request.getParameter("name"), Whitelist.none());
     String email = request.getParameter("email");
-    String message = request.getParameter("message");
+    String message = Jsoup.clean(request.getParameter("message"), Whitelist.none());
 
     // Print the value so you can see it in the server logs.
     System.out.println("New Name: " + name);
     System.out.println("New email: " + email);
     System.out.println("New message: " + message);
+    
+    long timestamp = System.currentTimeMillis();
 
+    Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
+    KeyFactory keyFactory = datastore.newKeyFactory().setKind("Message");
+     FullEntity taskEntity =
+        Entity.newBuilder(keyFactory.newKey())
+            .set("name", name)
+            .set("message", message)
+            .set("timestamp",timestamp )
+            .build();
+    datastore.put(taskEntity);
+    response.sendRedirect("/index.html");
+
+
+    
     response.setContentType("text/html;");
     // Write the value to the response so the user can see it.
     
--- portfolio/src/main/webapp/js.js
+++ portfolio/src/main/webapp/js.js
@@ -16,6 +16,58 @@ for (let i = 0; i < tags.length; i++) {
         }
 
     })
+}
+
+function sleep(){
+    var start = new Date().getTime();
+    while (new Date().getTime() < start + 1000);
+}
+
+
+
+function getMessages(){
+    var messages = []
+    fetch ('/messages')
+    .then((response)=>response.json())
+    .then((result)=>{
+        console.log(result)
+        messages=result
+    })
+    .then(()=>{
+        if(document.getElementsByClassName("oneMessage").length==0){
+        for(let i=0; i<messages.length; i++){
+            console.log(messages[i])
+            var root=document.getElementById("listMessagesWrapper")
+            var oneMessage=document.createElement("div")
+            oneMessage.id=messages[i].id
+            oneMessage.className="oneMessage"
+            var name = document.createElement("h3")
+            name.innerHTML=messages[i].name+":"
+            var message=document.createElement("p")
+            message.innerHTML="    "+messages[i].message
+            var deleteBtn=document.createElement("button")
+            deleteBtn.innerHTML="🗑️"
+            deleteBtn.onclick=function(){
+                deleteMessage(messages[i].id)
+            }
+            oneMessage.appendChild(name)
+            oneMessage.appendChild(message)
+            oneMessage.appendChild(deleteBtn)
+            oneMessage.style.display="flex"
+            oneMessage.style.flexDirection="row"
+
+            root.appendChild(oneMessage)
+            
+         }
+        }
+    })
 
 }
 
+function deleteMessage(id){
+    const params = new URLSearchParams();
+    params.append('id', id);
+    fetch('/delete', {method: 'POST', body: params});
+    alert("Message deleted")
+    window.location.reload()
+}
